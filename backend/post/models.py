ad29c5a80e24333239cb07171a936b8d4a9e3499
gkgkgkgkdlel <gkgkgkgkdlel@naver.com>
2022-10-05 01:43:49 +0900

    Post 모델에 삭제 여부를 판단하는 컬럼 추가

--- backend/post/models.py
+++ backend/post/models.py
@@ -10,6 +10,7 @@ class Post(models.Model):
     hashtags = models.ManyToManyField("HashTag", blank=True)
     like_count = models.IntegerField(default=0)
     view_count = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
