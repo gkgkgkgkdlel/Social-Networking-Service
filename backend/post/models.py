bfe2c6b97f151b0fa0fb3d0fab302dd2e7ddf448
gkgkgkgkdlel <gkgkgkgkdlel@naver.com>
2022-10-04 21:45:32 +0900

    Feat: Post, HashTag 모델 생성

--- backend/post/models.py
+++ backend/post/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    hashtags = models.ManyToManyField("HashTag", blank=True)
+    like_count = models.IntegerField(default=0)
+    view_count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "post"
+
+
+class HashTag(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "tag"
