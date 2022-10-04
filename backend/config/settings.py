15c1800962da91cc3cf29f659a539214466816d9
gkgkgkgkdlel <gkgkgkgkdlel@naver.com>
2022-10-04 20:55:58 +0900

    Feat: 회원가입, 로그인 api 추가

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+AUTH_USER_MODEL = "user.User"
 # Application definition
 
 INSTALLED_APPS = [
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "user",
+    "rest_framework.authtoken",
 ]
 
 MIDDLEWARE = [
@@ -117,3 +119,17 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.TokenAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticated",
+        # "rest_framework.authentication.SessionAuthentication",
+        # "rest_framework.authentication.BasicAuthentication",
+    ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
---
+++ backend/user/views.py
@@ -0,0 +1,63 @@
+from django.contrib.auth import authenticate
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import permission_classes
+from .serializers import UserSerializer
+import json
+import bcrypt
+import re
+from .models import User
+
+
+@permission_classes((AllowAny,))
+class SignupView(APIView):
+    def post(self, request):
+        """
+        회원가입 api.
+        """
+        email = request.data["email"]
+        password = request.data["password"]
+        name = request.data["name"]
+
+        user = User.objects.create_user(
+            email=email, password=password, name=name
+        )
+
+        user.save()
+
+        token = Token.objects.create(user=user)
+
+        return Response({"Token": token.key})
+
+
+@permission_classes((AllowAny,))
+class LoginView(APIView):
+    def post(self, request):
+        """
+        로그인 api.
+        """
+        email = request.data.get("email")
+        password = request.data.get("password")
+
+        if email is None or password is None:
+            return Response(
+                {"error": "Please provide both username and password"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        # 여기서 authenticate로 유저 validate
+        user = authenticate(request, email=email, password=password)
+
+        if not user:
+            return Response(
+                {"error": "Invalid credentials"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        # user 로 토큰 발행
+        token, _ = Token.objects.get_or_create(user=user)
+
+        return Response({"token": token.key}, status=status.HTTP_200_OK)
